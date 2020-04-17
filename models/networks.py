fb3fd5e4f5e67a6021daf151030ccf43e9b6a33e
Emmanuel Benazera <beniz@droidnik.fr>
2020-04-17 18:23:40 +0200

    added attngan + resnet G with attention

--- models/networks.py
+++ models/networks.py
@@ -160,6 +160,8 @@ def define_G(input_nc, output_nc, ngf, netG, norm='batch', use_dropout=False, in
         net = UnetGenerator(input_nc, output_nc, 7, ngf, norm_layer=norm_layer, use_dropout=use_dropout)
     elif netG == 'unet_256':
         net = UnetGenerator(input_nc, output_nc, 8, ngf, norm_layer=norm_layer, use_dropout=use_dropout)
+    elif netG == 'resnet_attn':
+        net = ResnetGenerator_attn(input_nc, output_nc, ngf, n_blocks=9)
     else:
         raise NotImplementedError('Generator model name [%s] is not recognized' % netG)
     return init_net(net, init_type, init_gain, gpu_ids)
@@ -401,6 +403,119 @@ class ResnetGenerator(nn.Module):
         """Standard forward"""
         return self.model(input)
 
+class ResnetGenerator_attn(nn.Module):
+    # initializers
+    def __init__(self, input_nc, output_nc, ngf=64, n_blocks=9):
+        super(ResnetGenerator_attn, self).__init__()
+        self.input_nc = input_nc
+        self.output_nc = output_nc
+        self.ngf = ngf
+        self.nb = n_blocks
+        self.conv1 = nn.Conv2d(input_nc, ngf, 7, 1, 0)
+        self.conv1_norm = nn.InstanceNorm2d(ngf)
+        self.conv2 = nn.Conv2d(ngf, ngf * 2, 3, 2, 1)
+        self.conv2_norm = nn.InstanceNorm2d(ngf * 2)
+        self.conv3 = nn.Conv2d(ngf * 2, ngf * 4, 3, 2, 1)
+        self.conv3_norm = nn.InstanceNorm2d(ngf * 4)
+
+        self.resnet_blocks = []
+        for i in range(n_blocks):
+            self.resnet_blocks.append(resnet_block(ngf * 4, 3, 1, 1))
+            self.resnet_blocks[i].weight_init(0, 0.02)
+
+        self.resnet_blocks = nn.Sequential(*self.resnet_blocks)
+
+        self.deconv1_content = nn.ConvTranspose2d(ngf * 4, ngf * 2, 3, 2, 1, 1)
+        self.deconv1_norm_content = nn.InstanceNorm2d(ngf * 2)
+        self.deconv2_content = nn.ConvTranspose2d(ngf * 2, ngf, 3, 2, 1, 1)
+        self.deconv2_norm_content = nn.InstanceNorm2d(ngf)
+        self.deconv3_content = nn.Conv2d(ngf, 27, 7, 1, 0)
+
+        self.deconv1_attention = nn.ConvTranspose2d(ngf * 4, ngf * 2, 3, 2, 1, 1)
+        self.deconv1_norm_attention = nn.InstanceNorm2d(ngf * 2)
+        self.deconv2_attention = nn.ConvTranspose2d(ngf * 2, ngf, 3, 2, 1, 1)
+        self.deconv2_norm_attention = nn.InstanceNorm2d(ngf)
+        self.deconv3_attention = nn.Conv2d(ngf, 10, 1, 1, 0)
+        
+        self.tanh = torch.nn.Tanh()
+
+    # weight_init
+    def weight_init(self, mean, std):
+        for m in self._modules:
+            normal_init(self._modules[m], mean, std)
+
+    # forward method
+    def forward(self, input):
+        x = F.pad(input, (3, 3, 3, 3), 'reflect')
+        x = F.relu(self.conv1_norm(self.conv1(x)))
+        x = F.relu(self.conv2_norm(self.conv2(x)))
+        x = F.relu(self.conv3_norm(self.conv3(x)))
+        x = self.resnet_blocks(x)
+        x_content = F.relu(self.deconv1_norm_content(self.deconv1_content(x)))
+        x_content = F.relu(self.deconv2_norm_content(self.deconv2_content(x_content)))
+        x_content = F.pad(x_content, (3, 3, 3, 3), 'reflect')
+        content = self.deconv3_content(x_content)
+        image = self.tanh(content)
+        image1 = image[:, 0:3, :, :]
+        # print(image1.size()) # [1, 3, 256, 256]
+        image2 = image[:, 3:6, :, :]
+        image3 = image[:, 6:9, :, :]
+        image4 = image[:, 9:12, :, :]
+        image5 = image[:, 12:15, :, :]
+        image6 = image[:, 15:18, :, :]
+        image7 = image[:, 18:21, :, :]
+        image8 = image[:, 21:24, :, :]
+        image9 = image[:, 24:27, :, :]
+        # image10 = image[:, 27:30, :, :]
+
+        x_attention = F.relu(self.deconv1_norm_attention(self.deconv1_attention(x)))
+        x_attention = F.relu(self.deconv2_norm_attention(self.deconv2_attention(x_attention)))
+        # x_attention = F.pad(x_attention, (3, 3, 3, 3), 'reflect')
+        # print(x_attention.size()) [1, 64, 256, 256]
+        attention = self.deconv3_attention(x_attention)
+
+        softmax_ = torch.nn.Softmax(dim=1)
+        attention = softmax_(attention)
+
+        attention1_ = attention[:, 0:1, :, :]
+        attention2_ = attention[:, 1:2, :, :]
+        attention3_ = attention[:, 2:3, :, :]
+        attention4_ = attention[:, 3:4, :, :]
+        attention5_ = attention[:, 4:5, :, :]
+        attention6_ = attention[:, 5:6, :, :]
+        attention7_ = attention[:, 6:7, :, :]
+        attention8_ = attention[:, 7:8, :, :]
+        attention9_ = attention[:, 8:9, :, :]
+        attention10_ = attention[:, 9:10, :, :]
+
+        attention1 = attention1_.repeat(1, 3, 1, 1)
+        # print(attention1.size())
+        attention2 = attention2_.repeat(1, 3, 1, 1)
+        attention3 = attention3_.repeat(1, 3, 1, 1)
+        attention4 = attention4_.repeat(1, 3, 1, 1)
+        attention5 = attention5_.repeat(1, 3, 1, 1)
+        attention6 = attention6_.repeat(1, 3, 1, 1)
+        attention7 = attention7_.repeat(1, 3, 1, 1)
+        attention8 = attention8_.repeat(1, 3, 1, 1)
+        attention9 = attention9_.repeat(1, 3, 1, 1)
+        attention10 = attention10_.repeat(1, 3, 1, 1)
+
+        output1 = image1 * attention1
+        output2 = image2 * attention2
+        output3 = image3 * attention3
+        output4 = image4 * attention4
+        output5 = image5 * attention5
+        output6 = image6 * attention6
+        output7 = image7 * attention7
+        output8 = image8 * attention8
+        output9 = image9 * attention9
+        # output10 = image10 * attention10
+        output10 = input * attention10
+
+        o=output1 + output2 + output3 + output4 + output5 + output6 + output7 + output8 + output9 + output10
+
+        return o, output1, output2, output3, output4, output5, output6, output7, output8, output9, output10, attention1,attention2,attention3, attention4, attention5, attention6, attention7, attention8,attention9,attention10, image1, image2,image3,image4,image5,image6,image7,image8,image9
+    
 
 class ResnetBlock(nn.Module):
     """Define a Resnet block"""
