bf7c9abaa3e999836feb1395be459e6c5b7329ee
Emmanuel Benazera <beniz@droidnik.fr>
2020-05-05 09:07:28 +0200

    added control of noise for D_noise and rec_noise

--- models/cycle_gan_semantic_mask_model.py
+++ models/cycle_gan_semantic_mask_model.py
@@ -47,9 +47,9 @@ class CycleGANSemanticMaskModel(BaseModel):
             parser.add_argument('--train_f_s_B', action='store_true', help='if true f_s will be trained not only on domain A but also on domain B')
             parser.add_argument('--fs_light',action='store_true', help='whether to use a light (unet) network for f_s')
             parser.add_argument('--lr_f_s', type=float, default=0.0002, help='f_s learning rate')
-            parser.add_argument('--D_noise', action='store_true', help='whether to add instance noise to discriminator inputs')
+            parser.add_argument('--D_noise', type=float, default=0.1, help='whether to add instance noise to discriminator inputs')
             parser.add_argument('--D_label_smooth', action='store_true', help='whether to use one-sided label smoothing with discriminator')
-            parser.add_argument('--rec_noise', action='store_true', help='whether to add noise to reconstruction')
+            parser.add_argument('--rec_noise', type=float, default=0.1, help='whether to add noise to reconstruction')
         return parser
     
     def __init__(self, opt):
@@ -187,19 +187,7 @@ class CycleGANSemanticMaskModel(BaseModel):
             #beniz: not adding optimizers f_s (?)
 
             self.rec_noise = opt.rec_noise
-            self.stddev = 0.1
             self.D_noise = opt.D_noise
-            #if opt.D_noise:   
-                #self.aug_seq = torch.nn.Sequential(kornia.augmentation.RandomAffine(degrees=[0.0,360.0],translate=[0.15,0.15],scale=[0.8,1.2],shear=[-0.1,0.1]),
-                #                                   kornia.augmentation.RandomHorizontalFlip(p=0.5),
-   
-   #                                                kornia.color.AdjustBrightness(np.random.uniform(0.0,0.1)),
-    #                                               kornia.color.AdjustContrast(np.random.uniform(0.9,1.0)),
-                                                   #kornia.color.AdjustSaturation(np.random.uniform(0.9,1.1)),
-                                                   #kornia.color.AdjustHue(np.random.uniform(-0.5,0.5)),
-                                                   #kornia.color.AdjustGamma(np.random.uniform(0.9,1.1)),
-     #                                              kornia.filters.GaussianBlur2d((5,5),(10.0,10.0)))
-
             
     def set_input(self, input):
         AtoB = self.opt.direction == 'AtoB'
@@ -221,15 +209,15 @@ class CycleGANSemanticMaskModel(BaseModel):
         d = 1
 
         if self.isTrain:
-            if self.rec_noise:
-                self.fake_B_noisy1 = self.gaussian(self.fake_B)
+            if self.rec_noise > 0.0:
+                self.fake_B_noisy1 = self.gaussian(self.fake_B, self.rec_noise)
                 self.rec_A= self.netG_B(self.fake_B_noisy1)
             else:
                 self.rec_A = self.netG_B(self.fake_B)
             
             self.fake_A = self.netG_B(self.real_B)
-            if self.rec_noise:
-                self.fake_A_noisy1 = self.gaussian(self.fake_A)
+            if self.rec_noise > 0.0:
+                self.fake_A_noisy1 = self.gaussian(self.fake_A, self.rec_noise)
                 self.rec_B = self.netG_A(self.fake_A_noisy1)
             else:
                 self.rec_B = self.netG_A(self.fake_A)
@@ -266,9 +254,9 @@ class CycleGANSemanticMaskModel(BaseModel):
                     self.real_A_mask = self.real_A #* label_A_in + self.real_A_out_mask
                     self.fake_B_mask = self.fake_B_mask_in + self.real_A_out_mask.float()
                     
-                if self.D_noise:
-                    self.fake_B_noisy = self.gaussian(self.fake_B)
-                    self.real_A_noisy = self.gaussian(self.real_A)
+                if self.D_noise > 0.0:
+                    self.fake_B_noisy = self.gaussian(self.fake_B, self.D_noise)
+                    self.real_A_noisy = self.gaussian(self.real_A, self.D_noise)
                     #self.real_A_mask_in = self.aug_seq(self.real_A_mask_in)
                     #self.fake_B_mask_in = self.aug_seq(self.fake_B_mask_in)
                     #self.real_A_mask = self.aug_seq(self.real_A_mask)
@@ -290,9 +278,9 @@ class CycleGANSemanticMaskModel(BaseModel):
                         self.real_B_mask = self.real_B #* label_B_in + self.real_B_out_mask
                         self.fake_A_mask = self.fake_A_mask_in + self.real_B_out_mask.float()
 
-                    if self.D_noise:
-                        self.fake_A_noisy = self.gaussian(self.fake_A)
-                        self.real_B_noisy = self.gaussian(self.real_B)
+                    if self.D_noise > 0.0:
+                        self.fake_A_noisy = self.gaussian(self.fake_A, self.D_noise)
+                        self.real_B_noisy = self.gaussian(self.real_B, self.D_noise)
                         #self.real_B_mask_in = self.aug_seq(self.real_B_mask_in)
                         #self.fake_A_mask_in = self.aug_seq(self.fake_A_mask_in)
                         #self.real_B_mask = self.aug_seq(self.real_B_mask)
@@ -330,7 +318,7 @@ class CycleGANSemanticMaskModel(BaseModel):
         self.loss_f_s.backward()
 
     def backward_D_A(self):
-        if self.D_noise:
+        if self.D_noise > 0.0:
             fake_B = self.fake_B_pool.query(self.fake_B_noisy)
             self.loss_D_A = self.backward_D_basic(self.netD_A, self.real_B_noisy, fake_B)
         else:
@@ -338,7 +326,7 @@ class CycleGANSemanticMaskModel(BaseModel):
             self.loss_D_A = self.backward_D_basic(self.netD_A, self.real_B, fake_B)
 
     def backward_D_B(self):
-        if self.D_noise:
+        if self.D_noise > 0.0:
             fake_A = self.fake_A_pool.query(self.fake_A_noisy)
             self.loss_D_B = self.backward_D_basic(self.netD_B, self.real_A_noisy, fake_A)
         else:
@@ -468,8 +456,7 @@ class CycleGANSemanticMaskModel(BaseModel):
         self.optimizer_f_s.zero_grad()
         self.backward_f_s()
         self.optimizer_f_s.step()
-
-    def gaussian(self, in_tensor):
-        noisy_image = torch.zeros(list(in_tensor.size())).data.normal_(0, self.stddev).cuda() + in_tensor
-        # noisy_tensor = 2 * (noisy_image - noisy_image.min()) / (noisy_image.max() - noisy_image.min()) - 1
+ 
+    def gaussian(self, in_tensor, stddev):
+        noisy_image = torch.zeros(list(in_tensor.size())).data.normal_(0, stddev).cuda() + in_tensor
         return noisy_image
